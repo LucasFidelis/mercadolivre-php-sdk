9bf5fb52560b95b302429fb34aa1f9dd75a492fa
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-05-18 12:11:48 -0300

    test: Client fetch me

--- tests/integration/Client/ClientTest.php
+++ tests/integration/Client/ClientTest.php
@@ -1,12 +1,18 @@
 <?php
 
 use LucasFidelis\MercadoLivreSdk\Client\Client;
-
+use LucasFidelis\MercadoLivreSdk\Entities\User;
 use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase {
   public function testCanBeCreated(): void {
     $client = new Client(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN);
     $this->assertInstanceOf(Client::class, $client);
+  }
+
+  public function testFetchMe() {
+    $client = new Client(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN);
+    $user = $client->getMe();
+    $this->assertInstanceOf(User::class, $user);
   }
 }
