0ee2ad33c4041da02e96d5676e53cd16cbaf18c5
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-05-20 18:20:04 -0300

    feat: find all by seller id

--- src/Managers/ProductManager.php
+++ src/Managers/ProductManager.php
@@ -8,6 +8,7 @@ use LucasFidelis\MercadoLivreSdk\Managers\Manager;
 class ProductManager extends Manager
 {
     protected static $FIND_BY_ID_URL = '/items/{itemId}/';
+    protected static $FIND_ALL_BY_SELLER_ID = '/users/{sellerId}/items/search';
 
     public function findById($itemId): Product
     {
@@ -15,5 +16,32 @@ class ProductManager extends Manager
         $response = $this->client->get($url);
         $data = json_decode($response, true);
         return Product::fromJson($data);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function findAllBySellerId(string $sellerId): array {
+        $products = [];
+        $total = 0;
+        $scrollId = '';
+        do {
+            $url = parent::factoryURL(self::$FIND_ALL_BY_SELLER_ID, ['sellerId' => $sellerId]);
+            $params = [
+                'search_type' => 'scan',
+                'limit' => 100,
+                'scroll_id' => $scrollId
+            ];
+            $url .= '?' . http_build_query($params);
+            $response = $this->client->get($url);
+            $data = json_decode($response, true);
+            $scrollId = $data['scroll_id'];
+            $total = $data['paging']['total'];
+            foreach ($data['results'] as $result) {
+                $products[] = $result;
+            }
+        } while (count($products) < $total);
+        
+        return $products;
     }
 }
