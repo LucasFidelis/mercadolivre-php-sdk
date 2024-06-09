9a35678d0bf86cdc7f2c89dca49b3f54b08c599d
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-09 12:43:16 -0300

    test: get Payments from Order

--- tests/unit/Entities/OrderTest.php
+++ tests/unit/Entities/OrderTest.php
@@ -3,6 +3,7 @@
 use LucasFidelis\MercadoLivreSdk\Entities\Order;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Buyer;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Coupon;
+use LucasFidelis\MercadoLivreSdk\Entities\Order\Payment;
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
@@ -33,5 +34,14 @@ class OrderTest extends TestCase
         $order = new Order($mockData);
         $this->assertInstanceOf(Buyer::class, $order->getBuyer());
         $this->assertEquals(85937594, $order->getBuyer()->getId());
+    }
+
+    public function testGetPayments(): void
+    {
+        $mockData = $this->getMockData('2000008464929496.json');
+        $order = new Order($mockData);
+        $payment = $order->getPayments()[0];
+        $this->assertIsArray($order->getPayments());
+        $this->assertInstanceOf(Payment::class, $payment);
     }
 }
