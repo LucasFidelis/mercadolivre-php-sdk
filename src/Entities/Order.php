7d0c4118cd2a53b9dbbb10ada899577cf9111035
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-09 12:43:03 -0300

    feat: add Order Payment collection

--- src/Entities/Order.php
+++ src/Entities/Order.php
@@ -4,15 +4,21 @@ namespace LucasFidelis\MercadoLivreSdk\Entities;
 
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Buyer;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Coupon;
+use LucasFidelis\MercadoLivreSdk\Entities\Order\Payment;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Seller;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Shipping;
 
 class Order extends DataObject
 {
     protected array $schema = [
+        'payments' => 'array',
         'coupon' => Coupon::class,
         'buyer' => Buyer::class,
         'shipping' => Shipping::class,
         'seller' => Seller::class
+    ];
+
+    protected array $collections = [
+        'payments' => Payment::class
     ];
 }
---
+++ src/Entities/Order/Payment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LucasFidelis\MercadoLivreSdk\Entities\Order;
+
+use LucasFidelis\MercadoLivreSdk\Entities\DataObject;
+
+class Payment extends DataObject
+{
+    protected array $schema = [
+        'id' => 'integer'
+    ];
+}
