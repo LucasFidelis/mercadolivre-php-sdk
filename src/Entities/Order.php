9cdf0a8559291e40b7c97b29ca9bc62d4e880cfd
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-09 00:18:06 -0300

    feat: get Buyer

--- src/Entities/Order.php
+++ src/Entities/Order.php
@@ -2,11 +2,13 @@
 
 namespace LucasFidelis\MercadoLivreSdk\Entities;
 
+use LucasFidelis\MercadoLivreSdk\Entities\Order\Buyer;
 use LucasFidelis\MercadoLivreSdk\Entities\Order\Coupon;
 
 class Order extends DataObject
 {
     protected array $schema = [
-        'coupon' => Coupon::class
+        'coupon' => Coupon::class,
+        'buyer' => Buyer::class
     ];
 }
---
+++ src/Entities/Order/Buyer.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LucasFidelis\MercadoLivreSdk\Entities\Order;
+
+use LucasFidelis\MercadoLivreSdk\Entities\DataObject;
+
+class Buyer extends DataObject
+{
+}
