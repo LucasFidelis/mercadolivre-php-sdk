bd35b3fa651b8dfd2ef13c1388245e5626873912
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-08 21:53:12 -0300

    test: must get Order by ID

---
+++ tests/integration/Manager/OrderManagerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use LucasFidelis\MercadoLivreSdk\Client\Client;
+use LucasFidelis\MercadoLivreSdk\Managers\OrderManager;
+use LucasFidelis\MercadoLivreSdk\Entities\Order;
+use PHPUnit\Framework\TestCase;
+
+class OrderManagerTest extends TestCase
+{
+    /**
+     * @var OrderManager
+     */
+    private Client $clientStub;
+    private OrderManager $sut;
+
+    public function setUp(): void
+    {
+        $this->clientStub = $this->createStub(Client::class);
+        $this->sut = new OrderManager($this->clientStub);
+    }
+
+    private function getMockData(string $filename) 
+    {
+        $data = file_get_contents(__DIR__ . '/../../resources/mock/orders/' . $filename);
+        return $data;
+    }
+
+    public function testMustGetOrderById(): void
+    {
+        $this->clientStub->method('get')
+            ->willReturn($this->getMockData('2000008464929496.json'));
+        $order =  $this->sut->getOrderById('2000008464929496');
+        $this->assertInstanceOf(Order::class, $order);
+        $this->assertEquals(90, $order->getTotalAmount());
+    }
+}
