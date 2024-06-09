50ab6008f16162c9ffa1e26ebe12ecd0b5094235
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-09 00:12:13 -0300

    test: must create an Order

---
+++ tests/unit/Entities/OrderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use LucasFidelis\MercadoLivreSdk\Entities\Order;
+use PHPUnit\Framework\TestCase;
+
+class OrderTest extends TestCase
+{
+    private function getMockData(string $filename): array
+    {
+        $data = file_get_contents(__DIR__ . '/../../resources/mock/orders/' . $filename);
+        return json_decode($data, true);
+    }
+
+    public function testCanBeCreated(): void
+    {
+        $mockData = $this->getMockData('2000008464929496.json');
+        $order = new Order($mockData);
+        $this->assertInstanceOf(Order::class, $order );
+    }
+}
