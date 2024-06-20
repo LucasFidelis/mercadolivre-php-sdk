b641e47374f9b2d3235db4f8a5306425b2e62840
Lucas Fidelis <lucasfidelis97@gmail.com>
2024-06-20 13:16:57 -0300

    feat: add get shipment label as ZPL

--- src/Managers/ShipmentManager.php
+++ src/Managers/ShipmentManager.php
@@ -6,12 +6,14 @@ use LucasFidelis\MercadoLivreSdk\Entities\Shipment;
 use LucasFidelis\MercadoLivreSdk\Entities\ShipmentCosts;
 use LucasFidelis\MercadoLivreSdk\Entities\ShipmentItem;
 use LucasFidelis\MercadoLivreSdk\Managers\Manager;
+use ZipArchive;
 
 class ShipmentManager extends Manager
 {
     protected static $GET_SHIPMENT_BY_ID_URL = '/shipments/{shipmentId}';
     protected static $GET_SHIPMENT_COSTS = '/shipments/{shipmentId}/costs';
     protected static $GET_SHIPMENT_ITEMS = '/shipments/{shipmentId}/items';
+    protected static $GET_SHIPMENT_LABEL = '/shipment_labels?shipment_ids={shipmentId}&response_type={responseType}';
 
     public function getShipmentById(int $shipmentId): Shipment
     {
@@ -40,5 +42,26 @@ class ShipmentManager extends Manager
         return array_map(static function ($shipmentItemData) {
             return new ShipmentItem($shipmentItemData);
         }, $data);
+    }
+
+    public function getShipmentLabelAsZPL(int $shipmentId): string
+    {
+        $url = parent::factoryURL(self::$GET_SHIPMENT_LABEL, ['shipmentId' => $shipmentId, 'responseType' => 'zpl2']);
+        $headers = ['Content-Type: application/zip'];
+        $response = $this->client->get($url, $headers);
+        return  $this->createShipmentLabelAtTemp($shipmentId, $response, 'Etiqueta de envio.txt');
+    }
+
+    private function createShipmentLabelAtTemp(int $shipmentId, string $zipContent, string $entryName): string
+    {
+        $tempDir = sys_get_temp_dir();
+        $filename = "$tempDir/$shipmentId.zip";
+        file_put_contents($filename, $zipContent);
+        $zip = new ZipArchive;
+        $zip->open($filename);
+        $content = $zip->getFromName($entryName);
+        $zip->close();
+        unlink($filename);
+        return $content;
     }
 }
